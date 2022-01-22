60bbf52baf4b2ca22dbcf41f0a76fa63cabc9149
Andrey Zakharenkov <zahaand@gmail.com>
2022-01-23 01:32:18 +0300

    Lesson03 realize Template Method in AbstractArrayStorage

--- src/com/zahaand/webapp/storage/SortedArrayStorage.java
+++ src/com/zahaand/webapp/storage/SortedArrayStorage.java
@@ -13,7 +13,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void sortStorage(String uuid) {
-        if (size > 1) {
+        if (size > 0) {
             int index = Math.abs(getIndex(uuid));
             System.arraycopy(storage, index, storage, index + 1, size - index);
             System.out.println("ARRAY SORTED");
