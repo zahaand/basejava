7703b688e6b3f35049bc203358cb6a30db1e686d
Andrey Zakharenkov <zahaand@gmail.com>
2022-01-30 00:39:54 +0300

    Lesson04 optimize imports

--- test/com/zahaand/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/zahaand/webapp/storage/AbstractArrayStorageTest.java
@@ -8,8 +8,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class AbstractArrayStorageTest {
     private final ArrayStorage storage = new ArrayStorage();
 
--- test/com/zahaand/webapp/storage/ArrayStorageTest.java
+++ test/com/zahaand/webapp/storage/ArrayStorageTest.java
@@ -1,6 +1,5 @@
 package com.zahaand.webapp.storage;
 
-import com.zahaand.webapp.exception.ExistStorageException;
 import com.zahaand.webapp.model.Resume;
 import org.junit.Assert;
 import org.junit.Before;
