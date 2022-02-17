3a757b59622f1029b926247a8d998a70ce630ac6
Andrey Zakharenkov <zahaand@gmail.com>
2022-02-17 15:06:28 +0300

    Lesson05 override methods

--- src/com/zahaand/webapp/storage/MapStorage.java
+++ src/com/zahaand/webapp/storage/MapStorage.java
@@ -1,5 +1,7 @@
 package com.zahaand.webapp.storage;
 
+import com.zahaand.webapp.exception.ExistStorageException;
+import com.zahaand.webapp.exception.NotExistStorageException;
 import com.zahaand.webapp.model.Resume;
 
 import java.util.HashMap;
@@ -9,34 +11,100 @@ public class MapStorage extends AbstractStorage {
     private final Map<String, Resume> storage = new HashMap<>();
 
     @Override
+    public void update(Resume r) {
+        String key = r.getUuid();
+        if (storage.containsKey(key)) {
+            storage.remove(key);
+            storage.put(key, r);
+            System.out.println(key + " SUCCESSFULLY UPDATED");
+        } else {
+            throw new NotExistStorageException(key);
+        }
+    }
+
+    @Override
+    public void save(Resume r) {
+        String key = r.getUuid();
+        if (storage.containsKey(key)) {
+            throw new ExistStorageException(key);
+        }
+        storage.put(r.getUuid(), r);
+        System.out.println(key + " SUCCESSFULLY SAVED");
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        if (storage.containsKey(uuid)) {
+            return storage.get(uuid);
+        } else {
+            throw new NotExistStorageException(uuid);
+        }
+    }
+
+    @Override
+    public void delete(String uuid) {
+        if (storage.containsKey(uuid)) {
+            storage.remove(uuid);
+            System.out.println(uuid + " SUCCESSFULLY DELETED");
+        } else {
+            throw new NotExistStorageException(uuid);
+        }
+    }
+
+    @Override
     protected void updateResume(Object searchKey, Resume resume) {
-        storage.replace((String) searchKey, resume);
+
     }
 
     @Override
     protected void saveResume(Resume resume) {
-        storage.put(resume.getUuid(), resume);
+
     }
 
     @Override
     protected Resume getResume(Object searchKey) {
-        String key = (String) searchKey;
-        return storage.get(key);
+        return null;
     }
 
     @Override
     protected void deleteResume(Object searchKey) {
-        storage.remove((String) searchKey);
+
     }
 
     @Override
     protected Object searchKey(Object uuid) {
-        String key = (String) uuid;
-        if (storage.containsKey(key)) {
-            return key;
-        }
-        return -1;
+        return null;
     }
+
+//    @Override
+//    protected void updateResume(Object searchKey, Resume resume) {
+//        storage.replace((String) searchKey, resume);
+//    }
+//
+//    @Override
+//    protected void saveResume(Resume resume) {
+//        storage.put(resume.getUuid(), resume);
+//    }
+//
+//    @Override
+//    protected Resume getResume(Object searchKey) {
+//        String key = (String) searchKey;
+//        return storage.get(key);
+//    }
+//
+//    @Override
+//    protected void deleteResume(Object searchKey) {
+//        storage.remove((String) searchKey);
+//    }
+//
+//    @Override
+//    protected Object searchKey(Object uuid) {
+//        String key = (String) uuid;
+//        if (storage.containsKey(key)) {
+//            return key;
+//        }
+//        return -1;
+//    }
 
     @Override
     public void clear() {
--- test/com/zahaand/webapp/storage/MapStorageTest.java
+++ test/com/zahaand/webapp/storage/MapStorageTest.java
@@ -1,20 +1,36 @@
 package com.zahaand.webapp.storage;
 
+import com.zahaand.webapp.exception.NotExistStorageException;
+import com.zahaand.webapp.model.Resume;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MapStorageTest extends AbstractStorageTest {
     public MapStorageTest() {
         super(new MapStorage());
     }
-//
-//    @Override
-//    @Test
-//    public void getAll() {
-//        assertEquals(3, storage.getAll().length);
-//    }
-//
-//    @Override
-//    @Test
-//    public void delete() {
-//        storage.delete("uuid1");
-//        assertEquals(2, storage.size());
-//    }
+
+    @Override
+    @Test
+    public void getAll() {
+        assertEquals(new Resume("uuid1"), storage.get("uuid1"));
+        assertEquals(new Resume("uuid2"), storage.get("uuid2"));
+        assertEquals(new Resume("uuid3"), storage.get("uuid3"));
+        assertEquals(3, storage.getAll().length);
+    }
+
+    @Override
+    @Test
+    public void delete() {
+        storage.delete("uuid1");
+        assertEquals(2, storage.size());
+    }
+
+    @Override
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() {
+        storage.delete("uuid1");
+        storage.get("uuid1");
+    }
 }
