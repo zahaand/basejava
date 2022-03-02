f0f2d808b1ffc64d428a2fc545f329076bba5450
Andrey Zakharenkov <zahaand@gmail.com>
2022-03-02 19:29:40 +0300

    Lesson06 fix bugs

--- src/com/zahaand/webapp/storage/SortedArrayStorage.java
+++ src/com/zahaand/webapp/storage/SortedArrayStorage.java
@@ -3,8 +3,9 @@ package com.zahaand.webapp.storage;
 import com.zahaand.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class SortedArrayStorage extends AbstractArrayStorage {
+public class SortedArrayStorage extends AbstractArrayStorage implements Comparator<Resume> {
     @Override
     protected void insertElement(Resume resume) {
         String uuid = resume.getUuid();
@@ -16,7 +17,12 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected Object getSearchKey(String uuid) {
         Resume resume = new Resume(uuid, "dummy");
-        return Arrays.binarySearch(storage, 0, size, resume, Resume::compareTo);
+        return Arrays.binarySearch(storage, 0, size, resume, Comparator.comparing(Resume::getUuid));
+    }
+
+    @Override
+    public int compare(Resume o1, Resume o2) {
+        return o1.getUuid().compareTo(o2.getUuid());
     }
 }
 
