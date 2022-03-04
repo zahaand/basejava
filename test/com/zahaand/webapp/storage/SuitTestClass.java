6812f262d6f0edd29205feaaa2bf818b55008c05
Andrey Zakharenkov <zahaand@gmail.com>
2022-03-04 18:22:36 +0300

    Lesson06 realize SuitTestClass

---
+++ test/com/zahaand/webapp/storage/SuitTestClass.java
@@ -0,0 +1,15 @@
+package com.zahaand.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapResumeStorageTest.class,
+        MapUuidStorageTest.class,
+})
+public class SuitTestClass {
+}
