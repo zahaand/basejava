0d3b712be1b79204160928a3350c96f175fbc95a
Andrey Zakharenkov <zahaand@gmail.com>
2022-03-27 18:25:51 +0300

    Lesson08 realize MainFile

--- src/com/zahaand/webapp/MainFile.java
+++ src/com/zahaand/webapp/MainFile.java
@@ -6,7 +6,8 @@ public class MainFile {
     public static void main(String[] args) {
         File directory = new File("/Users/zahaand/Java/basejava/src/com/zahaand/webapp");
         if (directory.isDirectory()) {
-            System.out.println("ROOT DIRECTORY " + directory.getName());
+            System.out.println("ROOT DIRECTORY: " + directory.getName());
+            System.out.println("PATH: " + directory.getPath());
             File[] files = directory.listFiles();
             if (files != null) {
                 for (File file : files) {
