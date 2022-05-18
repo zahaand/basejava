677a2e3a68196af55007eac46fa067d7d46c1e78
Andrey Zakharenkov <zahaand@gmail.com>
2022-05-18 11:13:04 +0300

    clean up code

--- test/com/zahaand/webapp/storage/MapResumeStorageTest.java
+++ test/com/zahaand/webapp/storage/MapResumeStorageTest.java
@@ -1,11 +1,6 @@
 package com.zahaand.webapp.storage;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class MapResumeStorageTest extends AbstractStorageTest{
-
+public class MapResumeStorageTest extends AbstractStorageTest {
     public MapResumeStorageTest() {
         super(new MapResumeStorage());
     }
