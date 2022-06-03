ca910d3d51b1d9f0c5ec658715aed176d71cc1f9
Andrey Zakharenkov <zahaand@gmail.com>
2022-06-04 00:46:31 +0300

    Lesson15 web

--- src/com/zahaand/webapp/web/ResumeServlet.java
+++ src/com/zahaand/webapp/web/ResumeServlet.java
@@ -1,5 +1,6 @@
 package com.zahaand.webapp.web;
 
+import com.zahaand.webapp.model.Resume;
 import com.zahaand.webapp.storage.Storage;
 
 import javax.servlet.ServletException;
@@ -7,47 +8,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
-//        String parameter = request.getParameter("uuid");
-//        Writer writer = response.getWriter();
-//        if (parameter == null) {
-//            writer.write(
-//                    "<html>" +
-//                            "<head>" +
-//                            "<title>All Resumes</title>" +
-//                            "</head>" +
-//                            "<body>" +
-//                            "<table>" +
-//                            "<tr>" +
-//                            "<th>uuid</th>" +
-//                            "<th>Full Name</th>" +
-//                            "</tr>");
-//            for (Resume resume : storage.getAllSorted()) {
-//                String uuid = resume.getUuid();
-//                String fullName = resume.getFullName();
-//                writer.write(
-//                        "<tr>" +
-//                                "<td>" + uuid + "</td>" +
-//                                "<td>" + fullName + "</td>" +
-//                                "</tr>"
-//                );
-//            }
-//            writer.write(
-//                    "</table>" +
-//                            "</body>" +
-//                            "</html>"
-//            );
-//        } else {
-//            writer.write(storage.get(parameter).toString());
-//        }
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String parameter = request.getParameter("uuid");
+        Writer writer = response.getWriter();
+        if (parameter == null) {
+            writer.write(
+                    "<html>" +
+                            "<head>" +
+                            "<title>All Resumes</title>" +
+                            "</head>" +
+                            "<body>" +
+                            "<table>" +
+                            "<tr>" +
+                            "<th>uuid</th>" +
+                            "<th>Full Name</th>" +
+                            "</tr>");
+            for (Resume resume : storage.getAllSorted()) {
+                String uuid = resume.getUuid();
+                String fullName = resume.getFullName();
+                writer.write(
+                        "<tr>" +
+                                "<td>" + uuid + "</td>" +
+                                "<td>" + fullName + "</td>" +
+                                "</tr>"
+                );
+            }
+            writer.write(
+                    "</table>" +
+                            "</body>" +
+                            "</html>"
+            );
+        } else {
+            writer.write(storage.get(parameter).toString());
+        }
     }
 
     @Override
